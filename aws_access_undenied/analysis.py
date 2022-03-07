a5854239c26a3d8666331b34f85f573e00cc91ae
NoamSDahan <noam@ermetic.com>
2022-03-07 12:30:44 +0200

    Separate analysis function from output

--- aws_access_undenied/analysis.py
+++ aws_access_undenied/analysis.py
@@ -22,7 +22,54 @@ from aws_access_undenied import simulate_custom_policy_helper
 from aws_access_undenied.results import AnalysisResult
 
 
-def _analyze_and_output_result(config: common.Config, raw_event: Dict[str, Any]) -> None:
+def _write_to_file(output_file: TextIO, output_json: Dict[str, Any]) -> None:
+    if output_file is not None and output_file:
+        output_file.write(json.dumps(output_json, indent=2))
+        output_file.close()
+
+
+def _handle_encoded_message(
+    config: common.Config, error_message: str
+) -> Optional[Dict[str, Any]]:
+    # Handle the case of EC2 encoded message, decode the message and output the result
+    # https://docs.aws.amazon.com/STS/latest/APIReference/API_DecodeAuthorizationMessage.html
+    logger.debug("Encoded EC2 Authorization error message present, attempt to decode")
+    return json.loads(
+        config.session.client("sts").decode_authorization_message(
+            EncodedMessage=error_message.split("Encoded authorization failure message: ")[
+                -1
+            ]
+        )["DecodedMessage"]
+    )
+
+
+def _is_valid_encoded_message(error_message) -> bool:
+    if error_message and "Encoded authorization failure message" in error_message:
+        if error_message[-3:] == "...":
+            logger.error(
+                "Could not decode authorization message because the message"
+                " in the CloudTrail event is incomplete. CloudTrail does"
+                " not store error messages longer than 1024 chars."
+                " Attempting regular analysis."
+            )
+            return False
+        return True
+
+
+def _write_to_output_buffer(
+    config: common.Config,
+    result: Union[Dict[str, Any], results.Result],
+) -> None:
+    """
+    Output result to file, stdout, or both - as per the configuration.
+    """
+    if not config.suppress_output:
+        print(str(result))
+    if config.output_file:
+        config.output_json.get("Results", []).append(json.loads(str(result)))
+
+
+def analyze(config: common.Config, raw_event: Dict[str, Any]) -> Optional[results.Result]:
     if raw_event.get("errorCode") not in [
         "AccessDenied",
         "Client.UnauthorizedOperation",
@@ -36,16 +83,12 @@ def _analyze_and_output_result(config: common.Config, raw_event: Dict[str, Any])
         return None
 
     event_ = event.Event(raw_event)
-    logger.info("AccessDenied cloudtrail_event found:" f" [eventID={event_.event_id}]")
+    logger.info(f"AccessDenied cloudtrail_event found: [eventID={event_.event_id}]")
     logger.debug(f"[errorMessage:{event_.error_message}]")
 
     if _is_valid_encoded_message(event_.error_message):
         try:
-            _write_to_output(
-                config,
-                _handle_encoded_message(config, event_.error_message),
-            )
-            return
+            return _handle_encoded_message(config, event_.error_message)
         except (
             errors.InvalidAuthorizationMessageException,
         ) as invalid_authorization_message_exception:
@@ -68,14 +111,10 @@ def _analyze_and_output_result(config: common.Config, raw_event: Dict[str, Any])
                 "Quick-exit condition: simple missing allow in identity-based"
                 " policy with all parameters known."
             )
-            _write_to_output(
-                config,
-                _create_simple_missing_allow_analysis_result(
-                    cloudtrail_event_=event_,
-                    event_permission_data_=event_permission_data_,
-                ),
+            return _create_simple_missing_allow_analysis_result(
+                cloudtrail_event_=event_,
+                event_permission_data_=event_permission_data_,
             )
-            return
 
         logger.debug("Gathering all relevant IAM policies for permission analysis...")
         iam_policy_data_ = iam_policy_data.IamPolicyData.from_event_permission_data(
@@ -114,22 +153,18 @@ def _analyze_and_output_result(config: common.Config, raw_event: Dict[str, Any])
             simulate_custom_policy_request,
         )
         if analysis_result:
-            _write_to_output(config, analysis_result)
-            return
+            return analysis_result
     except common.AccessUndeniedError as invalid_authorization_message_exception:
         logger.error(
             str(invalid_authorization_message_exception)
             + f" [eventID:{raw_event.get('eventID', '<None>')}]"
         )
-        _write_to_output(
-            config,
-            results.ErrorResult(
-                event_id=raw_event.get("eventID", "<None>"),
-                assessment_result=invalid_authorization_message_exception.access_denied_reason,
-                error_message=str(invalid_authorization_message_exception),
-            ),
+        return results.ErrorResult(
+            event_id=raw_event.get("eventID", "<None>"),
+            assessment_result=invalid_authorization_message_exception.access_denied_reason,
+            error_message=str(invalid_authorization_message_exception),
         )
-        return
+
     except (
         errors.AccessDenied,
         botocore.exceptions.ClientError,
@@ -138,69 +173,19 @@ def _analyze_and_output_result(config: common.Config, raw_event: Dict[str, Any])
             repr(invalid_authorization_message_exception)
             + f" [eventID:{raw_event.get('eventID', '<None>')}]"
         )
-        _write_to_output(
-            config,
-            results.ErrorResult(
-                event_id=raw_event.get("eventID", "<None>"),
-                assessment_result=common.AccessDeniedReason.ERROR,
-                error_message=str(invalid_authorization_message_exception),
-            ),
+        return results.ErrorResult(
+            event_id=raw_event.get("eventID", "<None>"),
+            assessment_result=common.AccessDeniedReason.ERROR,
+            error_message=str(invalid_authorization_message_exception),
         )
-        return
-
-
-def _write_to_file(output_file: TextIO, output_json: Dict[str, Any]) -> None:
-    if output_file is not None and output_file:
-        output_file.write(json.dumps(output_json, indent=2))
-        output_file.close()
-
-
-def _handle_encoded_message(
-    config: common.Config, error_message: str
-) -> Optional[Dict[str, Any]]:
-    # Handle the case of EC2 encoded message, decode the message and output the result
-    # https://docs.aws.amazon.com/STS/latest/APIReference/API_DecodeAuthorizationMessage.html
-    logger.debug("Encoded EC2 Authorization error message present, attempt to decode")
-    return json.loads(
-        config.session.client("sts").decode_authorization_message(
-            EncodedMessage=error_message.split("Encoded authorization failure message: ")[
-                -1
-            ]
-        )["DecodedMessage"]
-    )
-
-
-def _is_valid_encoded_message(error_message) -> bool:
-    if error_message and "Encoded authorization failure message" in error_message:
-        if error_message[-3:] == "...":
-            logger.error(
-                "Could not decode authorization message because the message"
-                " in the CloudTrail event is incomplete. CloudTrail does"
-                " not store error messages longer than 1024 chars."
-                " Attempting regular analysis."
-            )
-            return False
-        return True
-
-
-def _write_to_output(
-    config: common.Config,
-    result: Union[Dict[str, Any], results.Result],
-) -> None:
-    """
-    Output result to file, stdout, or both - as per the configuration.
-    """
-    if not config.suppress_output:
-        print(str(result))
-    if config.output_file:
-        config.output_json.get("Results", []).append(json.loads(str(result)))
 
 
 def analyze_cloudtrail_events(config: common.Config, raw_events_file_path):
     raw_events = json.load(raw_events_file_path)
     # Multiple CloudTrail log records in file or single event
     for raw_event in raw_events.get("Records") or [raw_events]:
-        _analyze_and_output_result(config, raw_event)
+        result = analyze(config, raw_event)
+        _write_to_output_buffer(config, result)
     _write_to_file(config.output_file, config.output_json)
 
 
