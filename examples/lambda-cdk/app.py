1bcb38fc248aa6d7b0e3bb9d841f04a5d3365bda
Jon Holman <github@jonholman.com>
2022-04-25 23:39:13 -0400

    Remove the CDK Aspect I had added to rename logical IDs

--- examples/lambda-cdk/app.py
+++ examples/lambda-cdk/app.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python3
 import aws_cdk as cdk
 from lambda_cdk.lambda_cdk_stack import LambdaCdkStack
-from lambda_cdk.update_logical_id_aspect import UpdateLogicalIdAspect
 
 app = cdk.App()
 LambdaCdkStack(app, "AccessUndeniedLambdaCdkStack")
-
-cdk.Aspects.of(app).add(UpdateLogicalIdAspect())
 
 app.synth()
